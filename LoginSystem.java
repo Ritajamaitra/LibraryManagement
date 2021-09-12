36d836ae01a6fedc6738884e921fe101fc37e0ac
Ritajamaitra <75163189+Ritajamaitra@users.noreply.github.com>
2021-09-12 07:41:11 +0530

    Delete LoginSystem.java

--- LoginSystem.java
+++
@@ -1,6 +0,0 @@
-public class LoginSystem {
-    public static  void main(String[] args) throws Exception {
-       IdandPasswords idandPasswords = new IdandPasswords();
-       new LoginInfo(( idandPasswords).getLoginInfo());
-    }
-}
